2ee316798005c1dd4b00c5d8f48ed79239ca020c
kcastr16 <kevincastrog28@gmail.com>
2024-10-05 22:47:25 -0400

    Updated with comments.
    Updated the file with comments explaining each section and elements utilized and their function.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Gets the input from the user */
+/* Gets the input from the user by identifying the ID for the search-input. The users input is then stored and added to a URL, which completes a accurrate google search. */
 function handleSubmit() {
   let search = document.getElementById("search-input").value;
   if (search) {
